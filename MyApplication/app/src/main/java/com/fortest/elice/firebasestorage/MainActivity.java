aeff154f0a20beaa13ca99b65b40baa48ee1fa0c
Elice-kim <grit_julia@naver.com>
2017-08-04 13:26:45 +0900

    add manifest permission about camera

--- MyApplication/app/src/main/java/com/fortest/elice/firebasestorage/MainActivity.java
+++ MyApplication/app/src/main/java/com/fortest/elice/firebasestorage/MainActivity.java
@@ -1,13 +1,30 @@
 package com.fortest.elice.firebasestorage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**open camera
+ * upload firebase storage
+ * and download image */
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.cameraFabBtn)
+    FloatingActionButton cameraFabBtn;
+    @BindView(R.id.storageImageView)
+    ImageView storageImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+
 }
